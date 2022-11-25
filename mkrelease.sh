fcf09305fa0a2710b66ba88793a13effc5891348
canwdev <c2097@live.com>
2022-11-25 21:03:41 +0800

    arrange folder

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #edit your ESP-IDF path here, tested with release/v3.3 branch
-export IDF_PATH=~/esp/esp-idf
+# export IDF_PATH="$HOME/esp/esp-idf/"
+# export PATH="$HOME/esp/xtensa-esp32-elf/bin:$PATH"
+# export PATH="$HOME/esp/esp-idf/tools:$PATH"
 
-#tune this to match yours
-export ESPLAY_SDK=~/esp/esplay-retro-emulation/esplay-sdk
-
-#ffmpeg path
-export PATH=/c/ffmpeg/bin:$PATH
+#ffmpeg -i main/gfxTile.png -f rawvideo -pix_fmt rgb565 main/gfxTile.raw -y
+#cat main/gfxTile.raw | xxd -i > main/gfxTile.inc
+#ffmpeg -i ./assets/Tile.png -f rawvideo -pix_fmt rgb565 ./assets/tile.raw -y
 
 cd esplay-launcher
 #make menuconfig
@@ -23,7 +23,14 @@ cd ../esplay-smsplusgx
 make -j4
 cd ..
 
-ffmpeg -i assets/Tile.png -f rawvideo -pix_fmt rgb565 assets/tile.raw -y
-/home/Peruri/esp/esplay-base-firmware/tools/mkfw/mkfw.exe Retro-Emulation assets/tile.raw 0 16 1310720 launcher esplay-launcher/build/RetroLauncher.bin 0 17 524288 esplay-nofrendo esplay-nofrendo/build/esplay-nofrendo.bin 0 18 458752 esplay-gnuboy esplay-gnuboy/build/esplay-gnuboy.bin 0 19 1179648 esplay-smsplusgx esplay-smsplusgx/build/esplay-smsplusgx.bin
-rm esplay-retro-emu.fw
-mv firmware.fw esplay-retro-emu.fw
+../esplay-base-firmware/tools/mkfw/mkfw Retro-Mod assets/tile.raw \
+    0 16 1310720 launcher esplay-launcher/build/RetroLauncher.bin
+
+# ../esplay-base-firmware/tools/mkfw/mkfw Retro-Mod assets/tile.raw \
+#     0 16 1310720 launcher esplay-launcher/build/RetroLauncher.bin \
+#     0 17 524288 esplay-nofrendo esplay-nofrendo/build/esplay-nofrendo.bin \
+#     0 18 458752 esplay-gnuboy esplay-gnuboy/build/esplay-gnuboy.bin \
+#     0 19 1179648 esplay-smsplusgx esplay-smsplusgx/build/esplay-smsplusgx.bin
+
+rm 001-esplay-retro-emu.fw
+mv firmware.fw 001-esplay-retro-emu.fw
